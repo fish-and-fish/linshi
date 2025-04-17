813c00d827b3506a05f75d46011e14142b1c4fce
yehuangyuan <ye.huangyuan@darcytech.com>
2025-04-17 16:41:10 +0800

    main

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Bean
+    public HttpClient getHttpClient() {
+        return HttpClientBuilder.create().build();
+    }
+
+
+}
